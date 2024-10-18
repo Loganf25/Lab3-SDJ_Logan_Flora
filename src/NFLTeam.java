d8eab138a13320ae42a59cad8979f3dbb09bde06
loganf25 <lflora@cub.uca.edu>
2024-10-18 12:21:37 -0500

    Added csv file reading support file and jar doc for parser utilized by apache.

--- src/NFLTeam.java
+++ src/NFLTeam.java
@@ -2,25 +2,91 @@ package src;
 
 public class NFLTeam {
     //Details for DetailsPanel
-    private String teamName;
-    private String key;
-    private int teamID;
-    private String city;
-    private String conference;
-    private String division;
-    private double AverageDraftPos;
-    private String headCoach;
-    private String colors;
-    private String logoURL;
+    private final String teamName;
     //Actual Data of Team
-    private int wins;
-    private int losses;
-    private int ties;
-    private int precent;
-    private int touchdowns;
-    private int pointsScored;
-    private int pointsAllowed;
-    private int streak;
+    //Game Stats (Calculate Win %)
+    private final int wins;
+    private final int losses;
+    //Points Stats (Calculate score difference)
+    private final int points_scored;
+    private final int points_allowed;
+    //Yards Stats
+    private final int totalYards;
+    private final int rushing;
+    private final int passing;
+    //Others (Calculate passing % and total tds)
+    private final int turnOvers;
+    private final int passes_att;
+    private final int passes_cmp;
+    private final int passing_tds;
+    private final int rushing_tds;
+    private final int penalties;
+
+    public NFLTeam(String teamName, int wins, int losses, int pointsScored, int pointsAllowed,
+                   int totalYards, int rushYds, int passYds, int turnovers, int passAtt, int passCmp, int passTd, int rushTd, int penalties) {
+        this.teamName = teamName;
+        this.wins = wins;
+        this.losses = losses;
+        this.points_scored = pointsScored;
+        this.points_allowed = pointsAllowed;
+        this.totalYards = totalYards;
+        this.rushing = rushYds;
+        this.passing = passYds;
+        this.turnOvers = turnovers;
+        this.passes_att = passAtt;
+        this.passes_cmp = passCmp;
+        this.passing_tds = passTd;
+        this.rushing_tds = rushTd;
+        this.penalties = penalties;
+    }
+
+    //Getters for Every private variable (Probably used in visuals)
+    //No setters as this is data from a file, never to be set by a user, only taken from file.
+    String getTeamName() {
+        return teamName;
+    }
+    int getWins() {
+        return wins;
+    }
+    int getLosses() {
+        return losses;
+    }
+    int getPointsScored() {
+        return points_scored;
+    }
+    int getPointsAllowed() {
+        return points_allowed;
+    }
+    int getTotalYards() {
+        return totalYards;
+    }
+    int getRushing() {
+        return rushing;
+    }
+    int getPassing() {
+        return passing;
+    }
+    int getTurnOvers() {
+        return turnOvers;
+    }
+    int getPassAtt() {
+        return passes_att;
+    }
+    int getPassCmp() {
+        return passes_cmp;
+    }
+    int getPassTds() {
+        return passing_tds;
+    }
+    int getRushingTds() {
+        return rushing_tds;
+    }
+    int getPenalties() {
+        return penalties;
+    }
+
+
+
 
 }
-}
+
--- src/vizDataBack.java
+++ src/vizDataBack.java
@@ -1,9 +1,58 @@
 package src;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 
 //The Data Collection side of the Program
 public class vizDataBack {
 
+    //An edited version of prof fileReader/parser to work with CVS files, as that's how my data is stored
+    public class CSVFileReader {
+        private String fileName;
+
+        public CSVFileReader (String fileName){
+            this.fileName = fileName;
+        }
+
+        public ArrayList<NFLTeam> getTeams() {
+            ArrayList<NFLTeam> teams = new ArrayList<>();
+
+            try(FileReader reader = new FileReader(fileName);
+                CSVParser csvParser = new CSVParser(reader,  CSVFormat.Builder.create().setHeader().build())){
+
+                for (CSVRecord csvRecord : csvParser)
+                {
+                    String teamName = csvRecord.get("team");
+                    int wins = Integer.parseInt(csvRecord.get("wins"));
+                    int losses = Integer.parseInt(csvRecord.get("losses"));
+                    int pointsScored = Integer.parseInt(csvRecord.get("points_scored"));
+                    int pointsAllowed = Integer.parseInt(csvRecord.get("points_allowed"));
+                    int totalYards = Integer.parseInt(csvRecord.get("total_yards"));
+                    int rushYds = Integer.parseInt(csvRecord.get("rush_yds"));
+                    int passYds = Integer.parseInt(csvRecord.get("pass_yds"));
+                    int turnovers = Integer.parseInt(csvRecord.get("turnovers"));
+                    int passAtt = Integer.parseInt(csvRecord.get("pass_att"));
+                    int passCmp = Integer.parseInt(csvRecord.get("pass_cmp"));
+                    int passTd = Integer.parseInt(csvRecord.get("pass_td"));
+                    int rushTd = Integer.parseInt(csvRecord.get("rush_td"));
+                    int penalties = Integer.parseInt(csvRecord.get("penalties"));
+                    NFLTeam team = new NFLTeam(teamName, wins, losses, pointsScored, pointsAllowed,
+                            totalYards, rushYds, passYds, turnovers, passAtt, passCmp, passTd, rushTd, penalties);
+                    teams.add(team);
+                }
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+            return teams;
+        }
+    }
 
 }
 
